744a3771387482acc78e8dd88f25f077293530db
EstebanG7297 <EstebanG7297@Fullsail.edu>
2014-01-28 23:40:24 -0500

    Professional Conditions w/o comments

--- Conditionals_Industry/conditionalsProfessional.js
+++ Conditionals_Industry/conditionalsProfessional.js
@@ -2,4 +2,18 @@
  * Created by Aeirus on 1/28/14.
  */
 
+var userPassword;
+var reInput;
+var whileCondition;
+
+userPassword = prompt("Please create a password");
+reInput = userPassword;
+
+userPassword = prompt("Please verify your password");
+
+
+while(whileCondition==false){
+    if(userPassword==reInput){whileCondition=true;}
+    else{userPassword = prompt("Incorrect. Please re enter your password");}
+}
 
