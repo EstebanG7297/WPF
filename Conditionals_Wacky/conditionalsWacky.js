e136620404a3f4647f0cc7ac9461f95e49092b7c
EstebanG7297 <EstebanG7297@Fullsail.edu>
2014-01-29 00:08:31 -0500

    added comments

--- Conditionals_Wacky/conditionalsWacky.js
+++ Conditionals_Wacky/conditionalsWacky.js
@@ -2,16 +2,20 @@
  * Created by Aeirus on 1/28/14.
  */
 
-var catCount;
-var experience;
+var catCount; //creates a place to store # of cats
+var experience; //creats a place to store how much experience you have
 
 catCount = prompt("Enter in a number of cats!!"); //asks the user to enter ina  # of cats for catCount
 parseInt(catCount);
 
 experience = prompt("Enter in how many years of experience you have juggling!!!"); //asks the user for # of years for experience
 
-var expNeeded = (catCount * 0.33) - experience;
+var expNeeded = (catCount * 0.33) - experience; //you need 1 year of expereince to juggle 3 cats. This determines how
+//your current experience allows you to juggle the # of cats
 
-if(expNeeded < 0){  console.log("you masterfully juggled " + catCount + " cats!!!");}
+if(expNeeded < 0){  console.log("you masterfully juggled " + catCount + " cats!!!");} //if your higher than the # of cats needed your a great
+//juggler
 else if(expNeeded == 0){ console.log("Listen Kid, you did alright out there, but get more at juggling experience."); }
+//if you only hit the quota your an ok juggler but need some practice
 else{ console.log("Sorry kid, you're too young to be juggling cats");}
+//you most likely tossed cats everywhere and lost 3 of them
