32efcb249fac09730c1a4634a7fe568a039d7ebf
EstebanG7297 <EstebanG7297@Fullsail.edu>
2014-01-29 00:21:25 -0500

    Industry tune up
    fixed scripting and naming issues, added comments

--- Conditionals_Industry/conditionalsIndustry.js
+++ Conditionals_Industry/conditionalsIndustry.js
@@ -2,18 +2,20 @@
  * Created by Aeirus on 1/28/14.
  */
 
-var userPassword;
-var reInput;
-var whileCondition;
+var userPassword; //creates a place to store the user password
+var reInput; //creates a place to store the password test
+var whileCondition = false; //makes sure that the following while loop will loop
 
-userPassword = prompt("Please create a password");
-reInput = userPassword;
+userPassword = prompt("Please create a password"); //user initially sets up value for their password
+reInput = userPassword; //the password test then holds onto this information
 
-userPassword = prompt("Please verify your password");
+userPassword = prompt("Please verify your password"); //user then has to re set their value for their password
 
 
 while(whileCondition==false){
-    if(userPassword==reInput){whileCondition=true;}
-    else{userPassword = prompt("Incorrect. Please re enter your password");}
+    if(userPassword==reInput){whileCondition=true;} // if they got their password right the loop breaks
+    else if(userPassword=="end"){break;} // user can end the program when they so desire
+    else{userPassword = prompt("Incorrect. Please re enter your password");} //if they get the password wrong
+    //they can attempt to re-enter it
 }
 
