d00cc8489b701a94e52066a2d31cc295f4593549
EstebanG7297 <EstebanG7297@Fullsail.edu>
2014-01-30 23:04:32 -0500

    Personal sub project addtion
    added second half to project

--- Functions_Personal/functionsPersonal.js
+++ Functions_Personal/functionsPersonal.js
@@ -25,3 +25,26 @@ function bmiCal(heightft,heightinch,weight){
 
     return bmi;
 }
+
+bmi = bmiCal(heightFT,heightIN,weight);
+
+console.log("your bmi is " + bmi);
+
+function ideaWeight(heightft,heightinch){
+
+    var idealBMI = 21 / 703;
+
+    heightft = heightft * 12;
+    heightinch = heightinch + heightft;
+    heightinch = heightinch * heightinch;
+
+    idealBMI = Math.floor(idealBMI * heightinch);
+
+    return idealBMI;
+}
+
+if(bmi>30){console.log("you are obese. You're ideal weight is " + ideaWeight(heightFT,heightIN) + " pounds")}
+else if(bmi<29.99 && bmi>25){console.log("you are over weight. You're ideal weight is " + ideaWeight(heightFT,heightIN) + " pounds")}
+else if(bmi<24.9 && bmi>18.6){console.log("you are your ideal weight")}
+else if(bmi<18.5 && bmi>18){console.log("you are thin for your height. You're ideal weight is " + ideaWeight(heightFT,heightIN) + " pounds")}
+else {console.log("you are underweight. You're ideal weight is " + ideaWeight(heightFT,heightIN) + " pounds")};
